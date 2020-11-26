caf096d6416590d1b379b6d30f2acf067dc33151
SultanSher <sultansheraliev7@gmail.com>
2020-11-27 02:42:24 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String homeWork = "fifteen";
+
+        final int CONST_NUM = 10;
+        String word = "five";
+
+        System.out.print("fifteen");
+        System.out.print(10);
+        System.out.print("five");
+
+
+        if (CONST_NUM < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        }
+
+        if (CONST_NUM > 0) {
+            System.out.println("Вы сохранили положительное число");
+        }
+
+    }
+}
+
+
